2162e1dde9884824668b80ea87f38c240849add4
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-02 18:12:34 -0300

    02: estrutura da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import Image from 'next/image'
+
+import camisetaOneImg from '../assets/camisetas/1.png'
+import camisetaTwoImg from '../assets/camisetas/2.png'
+import camisetaThreeImg from '../assets/camisetas/3.png'
+import { HomeContainer, Product } from '../styles/pages/home'
+
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camisetaOneImg} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camisetaTwoImg} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
